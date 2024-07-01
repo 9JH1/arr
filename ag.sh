1ac15bc2591040c85a7a01a028bbf5c33f11744c
9JH1 <tkf.x1os@gmail.com.>
2024-07-02 08:16:59 +1200

    fix before leaving

--- ag.sh
+++ ag.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 git add .
-echo "enter your commit message: "
 if [[ -v 1  ]]; then
+	echo "Your commit message: $1"
 	git commit -m "$1"
 else	
+	echo "Enter your commit message: "
 	read message
 	git commit -m "$message"
 
